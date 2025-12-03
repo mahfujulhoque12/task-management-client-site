18cd70df3f725e7ba4da13e7545a4a6051fba666
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-12-03 21:38:55 +0600

    add-code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-xl font-semibold text-red-400 text-center underline">
+      asdfasd
+    </div>
+  );
+}
+
+export default App;
